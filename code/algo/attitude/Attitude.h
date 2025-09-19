47f50f022614c23b2f8638dc195075640554b2e6
zerohzzzz <3190381602@qq.com>
2025-09-19 09:33:15 +0800

    feat: 新增姿态解算获取数据接口，后续装备将全局变量删除，改为静态变量

--- code/algo/attitude/Attitude.h
+++ code/algo/attitude/Attitude.h
@@ -19,6 +19,9 @@ typedef enum
 
 void attitude_init(Attitude_algorithm algo);
 void attitude_cal();
+float attitude_get_yaw();
+float attitude_get_pitch();
+float attitude_get_roll();
 
 extern euler_angle_t g_euler_angle;
 extern euler_angle_t g_euler_angle_bias;
